422b614628e7f7178ec5c2fb7da968ab49db67ff
Brad Lange <blange@gulllakecs.org>
2023-01-23 13:49:40 -0500

    Update welcome message

--- TheDeclarationOfIndependance.py
+++ TheDeclarationOfIndependance.py
@@ -2,3 +2,10 @@
 # Date: 1.20.2023
 # Program: Infotech Center Upgrades
 
+"""
+Our Welcome Screen will start our Program letting
+drivers know that the InfoTech Center 4.0 OS is loading
+"""
+
+print("\n\nWelcome - InfotechCenter 4.0")
+print("\nInfotech Center 4.0 OS is loading")

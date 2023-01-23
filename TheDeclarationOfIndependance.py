38298e75b1dbaa7ef2ed2a6368419066acf59199
Brad Lange <blange@gulllakecs.org>
2023-01-23 14:13:41 -0500

    Add Time.Sleep Library

--- TheDeclarationOfIndependance.py
+++ TheDeclarationOfIndependance.py
@@ -7,5 +7,11 @@ Our Welcome Screen will start our Program letting
 drivers know that the InfoTech Center 4.0 OS is loading
 """
 
+#Import Libraries Here
+import time
+
+timeToSleep = 2
+
 print("\n\nWelcome - InfotechCenter 4.0")
+time.sleep(timeToSleep)
 print("\nInfotech Center 4.0 OS is loading")

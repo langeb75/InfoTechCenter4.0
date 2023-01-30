37bf8bf2617caa3d4e992898d8418c72654bdf66
Brad Lange <blange@gulllakecs.org>
2023-01-30 13:27:59 -0500

    Update ... on Print Statement - Added Color - Changed Text & Spaces

--- TheDeclarationOfIndependance.py
+++ TheDeclarationOfIndependance.py
@@ -13,7 +13,7 @@ import sys  # I imported the system library for further use in code.
 
 timeToSleep = 2
 
-print("\n\nWelcome - InfotechCenter 4.0")
+print("\n\n\033[1;33;40mWelcome - InfotechCenter 4.0\n")
 time.sleep(timeToSleep)
 
 #print("\nInfotech Center 4.0 OS is loading")
@@ -22,11 +22,11 @@ a = 0
 
 while x != 20:
     x += 1
-    b = ("\033[1;33;40m Infotech Center 4.0 OS is loadin" + "." * a)
+    b = ("\033[1;33;40mInfotech Center 4.0 OS is loading" + "." * a)
     a = a + 1
     sys.stdout.write('\r'+b) # \r prints a carriage return first, so `b` is printed on top of the previous line.
     time.sleep(0.5)
     if a == 4:
         a = 0
     if x == 20:
-        print('\033[1;32;40m Done!')
+        print('\n\n\033[1;32;40mMission Accomplished - Retina Scanned - Access Granted')

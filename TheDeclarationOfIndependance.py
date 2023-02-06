5b43eb03c9785475894dbc5c83114166bc7629b0
Mr. Lange <blange@gulllakecs.org>
2023-02-06 13:57:49 -0500

    Updated Comments on DevTesting

--- TheDeclarationOfIndependance.py
+++ TheDeclarationOfIndependance.py
@@ -1,3 +1,45 @@
+# Programmer: Mr. Lange
+# Date Merged: 2.6.2023
+# Merged Welcome Screen and Gasoline Branches - Stable
+
+
+"""
+Our Welcome Screen will start our Program letting
+drivers know that the InfoTech Center 4.0 OS is loading
+"""
+
+#Import Libraries Here
+import time
+import sys  # I imported the system library for further use in code.
+import random
+from time import sleep
+
+timeToSleep = 2
+
+print("\n\n\033[1;33;40mWelcome - InfotechCenter 4.0\n")
+time.sleep(timeToSleep)
+
+
+x = 0
+a = 0
+
+while x != 20:
+    x += 1
+    b = ("\033[1;33;40mInfotech Center 4.0 OS is loading" + "." * a)
+    a = a + 1
+    sys.stdout.write('\r'+b) # \r prints a carriage return first, so `b` is printed on top of the previous line.
+    time.sleep(0.5)
+    if a == 4:
+        a = 0
+    if x == 20:
+        print('\n\n\033[1;32;40mMission Accomplished - Retina Scanned - Access Granted\n')
+
+
+
+
+
+
+
 # Programmer: Mr.Lange
 # Date: 1.30.2023
 # Program: Infotech Center 4.0 - Gasoline
@@ -16,9 +58,7 @@ Create a Function to determine our gas level and closest gas station
    - Print Closest Gas Station
 """
 
-# Import Libraries Here
-import random
-from time import sleep
+
 
 
 # Gas Level Function
@@ -66,32 +106,3 @@ def gasLevelAlert():
 gasLevelAlert()
 
 
-
-"""
-Our Welcome Screen will start our Program letting
-drivers know that the InfoTech Center 4.0 OS is loading
-"""
-
-#Import Libraries Here
-import time
-import sys  # I imported the system library for further use in code.
-
-timeToSleep = 2
-
-print("\n\n\033[1;33;40mWelcome - InfotechCenter 4.0\n")
-time.sleep(timeToSleep)
-
-#print("\nInfotech Center 4.0 OS is loading")
-x = 0
-a = 0
-
-while x != 20:
-    x += 1
-    b = ("\033[1;33;40mInfotech Center 4.0 OS is loading" + "." * a)
-    a = a + 1
-    sys.stdout.write('\r'+b) # \r prints a carriage return first, so `b` is printed on top of the previous line.
-    time.sleep(0.5)
-    if a == 4:
-        a = 0
-    if x == 20:
-        print('\n\n\033[1;32;40mMission Accomplished - Retina Scanned - Access Granted')
